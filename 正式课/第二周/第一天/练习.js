5eb830fb02fc1fd05aef808f82054ecbce961306
renjinhui <1227760869@qq.com>
2018-08-02 13:06:02 +0800

    m

--- 正式课/第二周/第一天/练习.js
+++ 正式课/第二周/第一天/练习.js
@@ -38,35 +38,29 @@ Fn.prototype.sum();
 
 
 function Foo() {
-    getName = function () {
-        console.log(1);
-    };
+    getName = function () {console.log(1);};
     return this;
 }
-Foo.getName = function () {
-    console.log(2);
-};
-Foo.prototype.getName = function () {
-    console.log(3);
-};
-var getName = function () {
-    console.log(4);
-};
-function getName() {
-    console.log(5);
-}
+Foo.getName = function () {console.log(2);}; // 当作对象
+Foo.prototype.getName = function () {console.log(3);};
+var getName = function () {console.log(4);};
+function getName() {console.log(5);}
 
-Foo.getName();
-getName();
-Foo().getName();
-getName();
-new Foo.getName();
-new Foo().getName();
-new new Foo().getName();
+Foo.getName(); // 2  ---》 当作普通函数执行
+getName(); //4
+Foo().getName(); // 1
+getName();// 1
+var a = new Foo.getName(); // 2  ---》 当作构造函数
+//  a 是 把 Foo 当作 对象时，调取到 他身上的 getName，把这个getName当作 构造函数执行，返回的 实例赋给 a
 
-
-
-
+var b = new Foo().getName();// new Foo() 返回的 Foo的实例 3
+// 先执行 new Foo； 返回Foo的一个实例 ，再通过 实例.getName 去找 getName这个属性，然后 把他执行
+var c = new new Foo().getName();
+// 限制性 new Foo; 返回 Foo的一个实例， 再通过 实例.getName 去找 getName这个属性；然后把它当作 构造函数执行
+// new Foo() 返回的  Foo的实例
+// new  Foo实例.getName()
+// c 是 getName 的 实例
+console.log(a,b,c);
 
 
 function Fn(){
