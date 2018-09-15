f0c67ae3e46a1d2712183065c24cb5fc5a60f22e
renjinhui <1227760869@qq.com>
2018-09-15 18:24:11 +0800

    m

--- 正式课/第八周/day4/todo/index.js
+++ 正式课/第八周/day4/todo/index.js
@@ -2,9 +2,27 @@ let vm = new Vue({
     el: '#app',
     data: {
         todo:'',
+        hash:'all',
         // count:1,
         // flag:false,
         ary:[{title:'示例',isSelect:false,isShow:false}] // 任物列表 isShow控制input显示
+    },
+    created(){
+        this.hash = location.hash.slice(2) || 'all';
+        //一进页面 就把数据从localStorage中获取；赋给 this.ary
+        let arr = JSON.parse(localStorage.getItem('myTodoData') || "[]");
+        this.ary = arr;
+        window.onhashchange = () => {
+            // console.log(location.hash);// #/all
+            this.hash = location.hash.slice(2);
+        }
+    },
+    directives:{
+        focus(el,obj){
+            if(obj.value){
+                el.focus();
+            }
+        }
     },
     computed:{
         count:{
@@ -17,7 +35,36 @@ let vm = new Vue({
                return arr.length;
            },
            set(){}
-        }
+        },
+        todoAry:{
+            // 这个数组 根据路由去返回不同列表
+            get(){
+                //需求 就是 只要ary发生改变  就要把 数据更新到 localStorage
+                localStorage.setItem('myTodoData',JSON.stringify(this.ary));
+                switch (this.hash){
+                    case 'all':
+                        //若是全部任务  则需要返回的数组包含所有的任务
+                        return this.ary;
+                        break;
+                    case 'finished':
+                        //若是完成的任务  则需要返回的数组包含所有的isSelect是true的项
+                        return this.ary.filter((item)=>{
+                            return item.isSelect;
+                        });
+                        break;
+                    case 'unfinished':
+                        return this.ary.filter((item)=>{
+                            return !item.isSelect
+                        });
+                        break;
+                }
+            },
+            set(){}
+        },
+        // localAry(){
+        //     console.log(2222);
+        //     return this.ary
+        // }
     },
     methods: {
         add(){
